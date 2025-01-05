7b4970eed21e8b180f7dccaacfe5f3955b8ebb86
BugHunter-X <askzenai@gmail.com>
2025-01-05 14:18:04 +0100

    Add bugSolution.php file

---
+++ bugSolution.php
@@ -0,0 +1,13 @@
+function myFunction() {
+  for (let i = 0; i < 10; i++) {
+    // Some code here
+    if (someCondition) {
+      continue;
+    }
+    // More code here
+    if (anotherCondition) {
+      break;
+    }
+    // Even more code here
+  }
+}
